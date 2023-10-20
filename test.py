61c391010a5b08d2eb12d3dfa8ecc0e7a75d89e3
Piyush Chugeja <piyushchugeja@gmail.com>
2023-10-20 19:28:11 +0530

    test words collection

---
+++ test.py
@@ -0,0 +1,37 @@
+import websocket
+import json
+import os
+
+chatId = os.urandom(16).hex()
+
+url = "wss://backend.buildpicoapps.com/api/chatbot/chat"
+
+websocket.enableTrace(False) 
+ws = websocket.WebSocketApp(url)
+systemPrompt = "You will be given a set of words, use those words to build a meaningful sentence. Return only the sentence and nothing more.";
+message = 'Generate a grammatically correct sentence using the following words: '
+words = ['peace', 'smile']
+language = 'English'
+message += ', '.join(words) + '. Use language: ' + language + '.'
+print(message)
+
+def on_open(ws):
+    payload = {
+        "chatId": chatId,
+        "appId": "dark-pay",
+        "systemPrompt": systemPrompt,
+        "message": message,
+    }
+    ws.send(json.dumps(payload))
+
+sentence = ''
+
+def on_message(ws, message):
+    global sentence
+    sentence += message
+
+ws.on_open = on_open
+ws.on_message = on_message
+ws.run_forever()
+print(sentence)
+ws.close()

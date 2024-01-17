8f38a7f45cb433e089fbbdad89a5ebbaf8f28724
Piyush Chugeja <piyushchugeja@gmail.com>
2024-01-17 23:29:50 +0530

    Update prompt

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ genai.configure(api_key=os.environ['GOOGLE_API_KEY'])
 model = genai.GenerativeModel("gemini-pro")
 
 def get_sentence(words, language):
-    prompt = "You are being given a set of words, generate a grammatically correct and meaningful sentence using those words. Do not add context to the sentence. Return only the sentence and nothing more. \nWords: " + ', '.join(words) + ". \nUse language: " + language + "."
+    prompt = "You are a sign language translator. You are being given a set of words collected from people using sign language, the words are what sentence they want to speak. Generate a grammatically correct and meaningful sentence using those words. Do not add context to the sentence. Return only the sentence and nothing more. \nWords: " + ', '.join(words) + ". \nUse language: " + language + "."
     response = model.generate_content(
         prompt,
     )
